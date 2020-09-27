6fc1be15e6f0280d9a55cdc89c98eaed65453876
Elvis Landi <elandi13@yahoo.com>
2020-09-27 18:16:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  dogs = ["pitbull", "poodle", "Pug", "Jack Russel"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    element << array 
 end
 
 def add_element_to_start_of_array(array, element)
